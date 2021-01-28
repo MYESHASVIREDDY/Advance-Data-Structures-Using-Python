b9801b767e1bc02aa81d328b19dbd025c25880cc
MYESHASVIREDDY <78152361+MYESHASVIREDDY@users.noreply.github.com>
2021-01-28 21:42:12 +0530

    Add files via upload

---
+++ Experiment 2a/merge_sort.py
@@ -0,0 +1,14 @@
+def merge_sort(alist, start, end):
+     if end - start > 1:
+        mid = (start + end)//2
+        merge_sort(alist, start, mid)
+        merge_sort(alist, mid, end)
+        merge_list(alist, start, mid, end)
+ 
+ 
+ 
+alist = input('Enter the list of numbers: ').split()
+alist = [int(x) for x in alist]
+merge_sort(alist, 0, len(alist))
+print('Sorted list: ', end='')
+print(alist)
